3cd51bdd9e70d99a316ae3c8c30512b550ce91e0
Bryan Yung <byung806@gmail.com>
2024-03-23 12:49:32 -0400

    Add workshop card to courses section

--- ui/components/sections/courses.tsx
+++ ui/components/sections/courses.tsx
@@ -1,8 +1,9 @@
+import WorkshopCard from "../workshop-card";
+
 export default function CoursesSection() {
     return (
         <section className="flex h-screen flex-col items-center justify-center bg-yellow-200">
-            <h2>Workshops</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <WorkshopCard imageUrl="/sinerider-bg.webp" title="Magnet Mysteries" description="Hello!" />
         </section>
     );
 }
