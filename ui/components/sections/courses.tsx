7915fb87b287d7e03433efe3b782e3db6ad32d33
Bryan Yung <byung806@gmail.com>
2024-03-25 21:54:09 -0400

    Add Magnet Mysteries example card

--- ui/components/sections/courses.tsx
+++ ui/components/sections/courses.tsx
@@ -2,8 +2,8 @@ import WorkshopCard from "../workshop-card";
 
 export default function CoursesSection() {
     return (
-        <section className="flex h-screen flex-col items-center justify-center bg-yellow-200">
-            <WorkshopCard imageUrl="/sinerider-bg.webp" title="Magnet Mysteries" description="Hello!" />
+        <section className="flex h-screen flex-col items-center justify-center">
+            <WorkshopCard imageUrl="/sinerider-bg.webp" title="Magnet Mysteries" description="Unlock the secrets of magnetism with our highly engaging magnet lab! With a lesson on the fundamentals of magnetism, followed by a series of stations with different magnet demonstrations, children can discover how magnets attract or repel certain materials and why." />
         </section>
     );
 }
