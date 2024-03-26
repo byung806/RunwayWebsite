92aefa1f09c970cc19269b6dc83a4f9cd8e72f0f
Bryan Yung <byung806@gmail.com>
2024-03-25 23:12:26 -0400

    Fix header inconsistency

--- ui/components/header.tsx
+++ ui/components/header.tsx
@@ -4,8 +4,8 @@ import Logo from '@/ui/components/logo';
 
 export default function Header() {
     return (
-        <nav className="sticky top-0 bg-white bg-opacity-60 backdrop-blur-md border-b border-gray-200">
-            <div className="mx-auto max-w-7xl px-3 sm:px-8">
+        <nav className="sticky top-0 w-full z-10 pt-1 pb-1 bg-white bg-opacity-60 backdrop-filter backdrop-blur-md border-b border-gray-200">
+            <div className="mx-auto max-w-7xl px-3 items-center">
                 <div className="flex h-16 justify-between gap-4 md:gap-8">
                     <Link className="flex" href="/">
                         <Logo />
