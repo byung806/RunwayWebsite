7fdc9aeb66a2b701be7b82e3c70938204110ea31
Jacob <jacoblokheenlee9@gmail.com>
2024-08-22 00:48:58 -0400

    gay email thing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
 import '@/ui/global.css';
 import { notosans } from '@/ui/fonts';
 import Header from '@/ui/components/header';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-            <body className={`${notosans.className} antialiased bg-black`}>
-                <Header />
-                {children}
-            </body>
-        </html>
+        <ChakraProvider>
+            <html lang="en">
+                <body className={`${notosans.className} antialiased bg-black`}>
+                    <Header />
+                    {children}
+                </body>
+            </html>
+        </ChakraProvider>
     );
 }
