917a68ac68f2fabda83937ca161f16951271936c
Bryan Yung <byung806@gmail.com>
2024-08-24 14:23:31 -0400

    Favicon & title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,16 @@ import '@/ui/global.css';
 import { notosans, lato, inter } from '@/ui/fonts';
 import Header from '@/ui/components/header';
 import Footer from '@/ui/components/footer';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Runway',
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={`${notosans.className} antialiased bg-[#fbf7ff] text-[#2e2e2e] font-bold`}>
+            <body className={`${notosans.className} antialiased bg-[#f9f7fa] text-[#2e2e2e]`}>
                 <Header />
                 {children}
                 <Footer />
