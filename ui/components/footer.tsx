646dab9fe29190edfbc28bb93ff72c7be3e83846
Bryan Yung <byung806@gmail.com>
2024-08-25 17:07:56 -0400

    Update borders

--- ui/components/footer.tsx
+++ ui/components/footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <footer className="z-10 flex w-full items-center justify-between border-b border-[#dedede] bg-[#f2f2f2] px-4 sm:px-14 md:px-28 py-1 sm:py-2 md:py-3 font-bold">
+        <footer className="z-10 flex w-full items-center justify-between bg-[#f2f2f2] px-4 sm:px-14 md:px-28 py-1 sm:py-2 md:py-3 font-bold">
             <Link
                 href="/privacy-policy"
                 className="rounded-full p-2 px-4 text-lg text-[#6b6b78] transition duration-300 hover:bg-[#f1f2f4] hover:text-[#2e2e2e]"
