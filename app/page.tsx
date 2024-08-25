fcc51bc043c57465577b43a1b8bc96e7bd58a8bb
Bryan Yung <byung806@gmail.com>
2024-08-25 15:25:19 -0400

    Push with debug borders
    [skip ci]

--- app/page.tsx
+++ app/page.tsx
@@ -55,16 +55,16 @@ export default function Page() {
             {/* Header height */}
             <div className="h-16"></div>
 
-            <section className="flex flex-col items-center space-y-8 px-4 py-10 md:flex-row md:space-x-8 md:space-y-0 md:px-10 md:py-40">
+            <section className="flex flex-col items-center space-y-8 border-2 border-black px-4 py-10 md:flex-row md:space-x-8 md:space-y-0 md:px-10 md:py-40">
                 <Image
-                    className="flex md:w-80 lg:w-[30rem]"
+                    className="flex border border-red-500 md:w-80 lg:w-[30rem]"
                     src={`${basePath}/ss-1-square.png`}
                     alt="phone screenshot"
                     width={300}
                     height={300}
                 />
-                <div className="flex flex-col items-center space-y-6 md:items-start">
-                    <h1 className="relative text-center text-3xl font-bold leading-[2.5rem] sm:leading-[2.5rem] md:w-full md:text-left md:text-4xl md:leading-[3rem] lg:text-4xl lg:leading-[3.5rem] xl:text-6xl xl:leading-[4.5rem]">
+                <div className="flex flex-col items-center space-y-6 border-2 border-fuchsia-500 md:items-start">
+                    <h1 className="relative border border-green-500 text-center text-3xl font-bold leading-[2.5rem] sm:leading-[2.5rem] md:w-full md:text-left md:text-4xl md:leading-[3rem] lg:text-4xl lg:leading-[3.5rem] xl:text-6xl xl:leading-[4.5rem]">
                         Learn about{' '}
                         <button
                             onClick={() => setIsPlaying(!isPlaying)}
@@ -92,10 +92,10 @@ export default function Page() {
                         <br />
                         in 2 minutes
                     </h1>
-                    <p className="text-center text-lg font-bold text-[#6b6b78] md:text-left">
+                    <p className="border border-yellow-500 text-center text-lg font-bold text-[#6b6b78] md:text-left">
                         The ultimate daily learning app, for iOS and Android.
                     </p>
-                    <form className="flex flex-col space-x-4 font-bold">
+                    <form className="flex flex-col space-x-4 border border-blue-500 font-bold">
                         {/* <Link href="/" className="px-10 bg-[#8b45a4] py-4 w-fit rounded-2xl transition duration-300 hover:scale-105">
                                     <p className="text-center text-white">Download, it's free</p>
                                 </Link> */}
@@ -109,13 +109,13 @@ export default function Page() {
                             </p>
                         </Link>
                     </form>
-                    <p className=" text-center text-lg font-bold text-[#6b6b78] md:text-left">
+                    <p className="border border-purple-500 text-center text-lg font-bold text-[#6b6b78] md:text-left">
                         Coming soon to the App Store and Google Play!
                     </p>
                 </div>
             </section>
 
-            <div className="mx-8 flex max-w-7xl flex-col gap-8 text-white md:flex-row">
+            <div className="mx-8 flex max-w-7xl flex-col gap-8 border border-purple-500 text-white md:flex-row">
                 <div className="flex h-full w-full flex-col items-center space-y-4 rounded-2xl bg-[#252525] bg-gradient-to-tr from-blue-600 to-cyan-600 p-10 text-center transition duration-300 hover:scale-105">
                     <h1 className="text-4xl font-bold">Habit-building.</h1>
                     <p className="text-2xl text-[#dac8da]">
